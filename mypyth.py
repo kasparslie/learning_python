972b44590cb68ff13acd04c45e20eac2043b2560
Kaspars <kasparslie@gmail.com>
2021-12-02 21:54:24 +0100

    course 1, week 6 push

--- mypyth.py
+++ mypyth.py
@@ -1,15 +1,15 @@
-# print("Hello world!")
-# nam = input("Who are you?")
-# print("Welcome", nam)
+print("Hello world!")
+nam = input("Who are you?")
+print("Welcome", nam)
 
-# hrs = input("Enter Hours:")
-# rte = input("Enter Rate:")
-# h = float(hrs)
-# r = float(rte)
-# if h <= 40:
-#     print(h * r)
-# elif h > 40:
-#     print((40 * r) + ((h-40)* (r * 1.5)))
+hrs = input("Enter Hours:")
+rte = input("Enter Rate:")
+h = float(hrs)
+r = float(rte)
+if h <= 40:
+    print(h * r)
+elif h > 40:
+    print((40 * r) + ((h-40)* (r * 1.5)))
 
 score = input("Enter Score: ")
 x = float(score)
@@ -25,3 +25,20 @@ elif x < 0.6:
     print("F")
 else:
     print("you are fucked")
+
+
+def computepay(h, r):
+    
+    a=0
+ 
+    if float(h) > 40:
+        a = (40 * r) + ((h-40)* (r * 1.5))
+    elif float(h) == 40:
+        a = (h * r)
+
+    return a
+
+hrs = input("Enter Hours:")
+rte = input("Enter Rate")
+p = computepay(float(hrs), float(rte))
+print("Pay", p)
