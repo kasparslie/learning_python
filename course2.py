bd2ea22ff633031c54764d9f895946c6a9c99cd7
Kaspars <kasparslie@gmail.com>
2021-12-09 23:01:52 +0100

    week 4 completed

--- course2.py
+++ course2.py
@@ -8,3 +8,57 @@ print(extc)
 
 num = float(extc)
 print(num)
+
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+fhandle = open(fname)
+for line in fhandle:
+    line = line.upper().rstrip()
+    print(line)
+
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+average = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    average += float(line[20:-1].strip())
+    count = count + 1
+    print(line)
+    
+print("Average spam confidence:", (average/count))
+
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    txt = line.rstrip().split()
+    # print(txt)
+    for wrd in txt:
+        # print(wrd)
+        if wrd in lst:
+          continue
+        else:
+            lst.append(wrd)
+lst.sort()
+print(lst)
+
+fname = input("Enter file name: ")
+fname = "mbox-short.txt"
+icount = 0
+fh = open(fname)
+
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    print(words[1])
+    icount +=1
+
+
+
+
+print("There were", icount, "lines in the file with From as the first word")
+
