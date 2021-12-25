adcf842a94b3f58f3adda71bd3e89efbe2c72339
Kaspars <kasparslie@gmail.com>
2021-12-25 21:06:03 +0100

    last commit to week7/course9

--- course2.py
+++ course2.py
@@ -1,72 +1,72 @@
-# text = "X-DSPAM-Confidence:    0.8475"
+text = "X-DSPAM-Confidence:    0.8475"
 
-# str = text.find(":")
-# print(str)
+str = text.find(":")
+print(str)
 
-# extc = text[str+1:]
-# print(extc)
+extc = text[str+1:]
+print(extc)
 
-# num = float(extc)
-# print(num)
+num = float(extc)
+print(num)
 
-# # Use words.txt as the file name
-# fname = input("Enter file name: ")
-# fhandle = open(fname)
-# for line in fhandle:
-#     line = line.upper().rstrip()
-#     print(line)
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+fhandle = open(fname)
+for line in fhandle:
+    line = line.upper().rstrip()
+    print(line)
 
-# # Use the file name mbox-short.txt as the file name
-# fname = input("Enter file name: ")
-# fh = open(fname)
-# count = 0
-# average = 0
-# for line in fh:
-#     if not line.startswith("X-DSPAM-Confidence:") : continue
-#     average += float(line[20:-1].strip())
-#     count = count + 1
-#     print(line)
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+average = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    average += float(line[20:-1].strip())
+    count = count + 1
+    print(line)
     
-# print("Average spam confidence:", (average/count))
+print("Average spam confidence:", (average/count))
 
-# fname = input("Enter file name: ")
-# fh = open(fname)
-# lst = list()
-# for line in fh:
-#     txt = line.rstrip().split()
-#     # print(txt)
-#     for wrd in txt:
-#         # print(wrd)
-#         if wrd in lst:
-#           continue
-#         else:
-#             lst.append(wrd)
-# lst.sort()
-# print(lst)
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    txt = line.rstrip().split()
+    # print(txt)
+    for wrd in txt:
+        # print(wrd)
+        if wrd in lst:
+          continue
+        else:
+            lst.append(wrd)
+lst.sort()
+print(lst)
 
-# fname = input("Enter file name: ")
-# fname = "mbox-short.txt"
-# icount = 0
-# fh = open(fname)
+fname = input("Enter file name: ")
+fname = "mbox-short.txt"
+icount = 0
+fh = open(fname)
 
-# for line in fh:
-#     line = line.rstrip()
-#     if not line.startswith('From '):
-#         continue
-#     words = line.split()
-#     print(words[1])
-#     icount +=1
-
-
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    print(words[1])
+    icount +=1
 
 
-# print("There were", icount, "lines in the file with From as the first word")
 
-# stuff = dict()
-# print(stuff['candy'])
 
-# stuff = dict()
-# print(stuff.get('candy',-1))
+print("There were", icount, "lines in the file with From as the first word")
+
+stuff = dict()
+print(stuff['candy'])
+
+stuff = dict()
+print(stuff.get('candy',-1))
 
 
 # Python Dictionaries and Files
@@ -85,7 +85,7 @@ print(newList)
 
 for email in newList:
     senders[email] = senders.get(email,0) +1
-# print(senders)
+print(senders)
 
 statWord = None
 statCount= None
@@ -95,3 +95,34 @@ for word,count in senders.items():
         statWord  = word
 
 print(statWord, statCount)
+
+x = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}
+y = x.items()
+# print(y)
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+newList = list()
+hCount = dict()
+for line in handle:
+    line=line.split()
+
+    if len(line) > 2 and line[0] == 'From':
+        hr = line[5].split(':')
+        hCount[hr[0]] = hCount.get(hr[0], 0) + 1
+       
+    else:
+        continue
+    
+for key, val in hCount.items():
+    newTuple = (key, val)
+    newList.append(newTuple)
+    newList = sorted(newList)
+
+for key, val in newList:
+    print(key, val)
+
+stuff = dict()
+print(stuff.get('candy',-1))
